30d9c7a96122c9c7224072399d6c753b0edf4877
panprogramadorgh <barreroalvaro2007@gmail.com>
2024-11-10 01:52:03 +0100

    Error deploy

--- src/utils/octokit.ts
+++ src/utils/octokit.ts
@@ -10,9 +10,9 @@ export async function getGithubUser() {
   try {
     const result = await octokit.request("GET /user", {})
     return result.data;
-  } catch (error: any) {
-    if (typeof error.status != 'undefined') {
-      console.error(`Error when fetching data from GitHub API: ${error.status} - ${error.response.data.message}`);
+  } catch (error) {
+    if (typeof (error as any).status != 'undefined') {
+      console.error(`Error when fetching data from GitHub API: ${(error as any).status} - ${(error as any).response.data.message}`);
     }
     console.error(error);
   }
