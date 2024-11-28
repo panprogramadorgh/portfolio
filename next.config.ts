dd7baa6479c7d4053225de25e518bc34aa1f42b4
panprogramadorgh <barreroalvaro2007@gmail.com>
2024-11-28 22:25:41 +0100

    Second deploy bug fix

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,15 @@ const nextConfig: NextConfig = {
         },
       }
     }
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
